c5c75bb43c421140d00d8f24588c44d3fc747d55
棚原　嘉 <ysm.t02@gmail.com>
2018-11-04 00:37:30 +0900

    DBの更新処理とrenderTodoListの振り分け処理

--- TodoHW/assets/js/app.js
+++ TodoHW/assets/js/app.js
@@ -2,34 +2,35 @@ let removeIcon = '<i class="far fa-trash-alt fa-lg"></i>';
 let doneIcon = '<i class="far fa-check-circle fa-lg"></i>';
 let data;
 
-if (localStorage.getItem('todoList')){
+if (localStorage.getItem('todoList')) {
   data = JSON.parse(localStorage.getItem('todoList'));
 
-  // renderTodoList();
+  renderTodoList();
 
 } else {
-
   data = {
     not:[],
     done:[]
   };
 }
-
 console.log(data);
 
-// 追加ボタンをクリックしたとき
+
+// 追加ボタンをクリックした時
   let add = document.getElementById('add');
   add.addEventListener('click',function() {
 
-// ユーザーが入力した内容を取得
-  let taskName = document.getElementById('task').value;
 
+  // ユーザーが入力した内容を取得
+  let taskName = document.getElementById('task').value;
   addTaskToDOM(taskName);
   document.getElementById('task').value = '';
   data.not.push(taskName);
   localStorage.setItem('todoList',JSON.stringify(data));
-  })
+})
 
+
+// doneとnot-yetに振り分ける
 function addTaskToDOM(taskName,isDone) {
   let list;
   if (isDone) {
@@ -37,27 +38,31 @@ function addTaskToDOM(taskName,isDone) {
   } else {
     list = document.getElementById('not-yet');
   }
-
-  let not = document.createElement('li');
+　let not = document.createElement('li');
   let text = document.createTextNode(taskName);
   not.appendChild(text);
 
+
   // ボタンを表示する場所
   let buttons = document.createElement('div');
-  buttons.classlist.add('buttons');
+  buttons.classList.add('buttons');
 
-  // 削除ボタンの作成
+
+  //削除ボタンを作成
   let remove = document.createElement('button');
-  remove.classlist.add('remove');
+  remove.classList.add('remove');
   remove.innerHTML = removeIcon;
+
 
   // 削除ボタンをクリック
   remove.addEventListener('click',removeTask);
 
-  // 完了ボタンの作成
+
+  // 完了ボタンを作成
   let done = document.createElement('button');
-  done.classlist.add('done');
+  done.classList.add('done');
   done.innerHTML = doneIcon;
+
 
   // 完了ボタンをクリック
   done.addEventListener('click',doneTask);
@@ -68,29 +73,32 @@ function addTaskToDOM(taskName,isDone) {
   list.appendChild(not);
 }
 
+// removeTaskを作成
 function removeTask() {
   let task = this.parentNode.parentNode;
   let id = task.parentNode.id;
   let value = task.textContent;
-
+  
   task.remove();
 
-  // DBから削除
+
+// DBから削除
   if (id === 'not-yet') {
     data.not.splice(data.not.indexOf(value),1);
-  } else {
+  }else {
     data.done.splice(data.done.indexOf(value),1);
   }
   dataObjectUpdate();
 }
 
+
+// doneTaskを作成
 function doneTask() {
   let task = this.parentNode.parentNode;
   let id = task.parentNode.id;
   if (id === 'done') {
     return;
   }
-
   let value = task.textContent;
 
   let target = document.getElementById('done');
@@ -99,4 +107,20 @@ function doneTask() {
   data.not.splice(data.not.indexOf(value),1);
   data.done.push(value);
   dataObjectUpdate();
+  }
+
+
+// DBの更新
+  function dataObjectUpdate() {
+  localStorage.setItem('todoList',JSON.stringify(data));
+  }
+
+
+  function renderTodoList() {
+    for (let taskName of data.not) {
+  addTaskToDOM(taskName,false);
+  }
+    for (let taskName of data.done) {
+  addTaskToDOM(taskName,true);
+  }
 }
